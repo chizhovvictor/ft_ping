d78e34c85b6dfeaded43a37a51ed8d5266236c9b
Victor Chizhov <victor.chizhov93@gmail.com>
2024-09-19 13:14:36 +0200

    WIP

--- src/ft_ping.c
+++ src/ft_ping.c
@@ -1,5 +1,23 @@
 #include "../include/ft_ping.h"
 
+char* error_message = "usage: ping [-AaDdfnoQqRrv] [-c count] [-G sweepmaxsize]\n"
+                      "            [-g sweepminsize] [-h sweepincrsize] [-i wait]\n"
+                      "            [-l preload] [-M mask | time] [-m ttl] [-p pattern]\n"
+                      "            [-S src_addr] [-s packetsize] [-t timeout][-W waittime]\n"
+                      "            [-z tos] host\n"
+                      "       ping [-AaDdfLnoQqRrv] [-c count] [-I iface] [-i wait]\n"
+                      "            [-l preload] [-M mask | time] [-m ttl] [-p pattern] [-S src_addr]\n"
+                      "            [-s packetsize] [-T ttl] [-t timeout] [-W waittime]\n"
+                      "            [-z tos] mcast-group\n"
+                      "Apple specific options (to be specified before mcast-group or host like all options)\n"
+                      "            -b boundif           # bind the socket to the interface\n"
+                      "            -k traffic_class     # set traffic class socket option\n"
+                      "            -K net_service_type  # set traffic class socket options\n"
+                      "            --apple-connect      # call connect(2) in the socket\n"
+                      "            --apple-time         # display current time\n";
+
+    
+
 void send_ping(int ping_sockfd, struct sockaddr_in *ping_addr, char *ping_ip, char *host)
 {
     int ttl_val = 64;
